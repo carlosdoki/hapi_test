54622d2b5a577b58717becf23f62af614ae3b7dc
Carlos Doki <carlosdoki@gmail.com>
2020-08-11 17:46:11 -0300

    commit

--- server.js
+++ server.js
@@ -3,7 +3,33 @@ import debug from "debug";
 import os from "os";
 import qs from "qs";
 import inert from "inert";
+import appdynamics from "appdynamics";
 
+appdynamics.profile({
+  controllerHostName: "ec2-54-214-166-126.us-west-2.compute.amazonaws.com",
+  controllerPort: 8090,
+  controllerSslEnabled: false, // Set to true if controllerPort is SSL
+  accountName: "customer1",
+  accountAccessKey: "16ff4175-95c7-4c2f-807d-fb083e398055", //required
+  applicationName: "hub-precos",
+  tierName: "export",
+  nodeName: "hub-precos-export",
+  reuseNode: true,
+  reuseNodePrefix: "hub-precos-export",
+  noNodeNameSuffix: false,
+  logging: {
+    logfiles: [
+      {
+        root_directory: "./logs",
+        filename: "echo_%N.log",
+        level: "DEBUG",
+        max_size: 5242880,
+        max_files: 10,
+        // outputType: "console", // Set this parameter if you want to log to STDOUT/STDERR. Omit this parameter if you want to log to a file.
+      },
+    ],
+  },
+});
 const log = debug("export:server");
 const error = debug("export:server:error");
 
